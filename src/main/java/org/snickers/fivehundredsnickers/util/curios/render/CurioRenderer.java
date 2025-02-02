596316aa91c2363f67b0e2dedbc00bf2d8a3e110
myNigga <snickers2618@ya.ru>
2025-02-02 22:33:48 +0300

    Made stogie follow head up and down

--- src/main/java/org/snickers/fivehundredsnickers/util/curios/render/CurioRenderer.java
+++ src/main/java/org/snickers/fivehundredsnickers/util/curios/render/CurioRenderer.java
@@ -18,12 +18,7 @@ import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
 
 public class CurioRenderer  implements ICurioRenderer {
-    private final BakedModel model;
-
-    public CurioRenderer() {
-        model = Minecraft.getInstance().getModelManager().getModel(new ResourceLocation(FiveHundredSnickers.MOD_ID, "item/stogie_2d"));
-
-    }
+    public CurioRenderer() {}
 
     @Override
     public <T extends LivingEntity, M extends EntityModel<T>> void render(ItemStack stack,
@@ -37,39 +32,23 @@ public class CurioRenderer  implements ICurioRenderer {
         if (entity.level().isClientSide && !Minecraft.getInstance().options.getCameraType().isFirstPerson()) {
             ItemRenderer renderer = Minecraft.getInstance().getItemRenderer();
             BakedModel model1 = renderer.getModel(stack, entity.level(), entity, 0);
-            matrixStack.scale(1, 1, 1);
-//            Quaternionf rotation = new Quaternionf(1, 1, 1, 1);
+            FiveHundredSnickers.LOGGER.info("headPitch="+headPitch+"; netHeadYaw="+netHeadYaw);
+//            Quaternionf rotation = new Quaternionf(-1, 0, 0, (headPitch/90)+1);
 
-//            matrixStack.mulPose(rotation);
-//            matrixStack.translate(0, 0.5, 0);
+            Quaternionf pitch = new Quaternionf().rotationX((float) Math.toRadians(headPitch-90));
+            matrixStack.rotateAround(pitch, 0, -0.1f, 0.0f);
+            matrixStack.scale(1, 1, 1);
+            matrixStack.translate(0, -0.1f, -0.05);
+
             renderer.render(stack, ItemDisplayContext.NONE, false, matrixStack, renderTypeBuffer, light, OverlayTexture.NO_OVERLAY, model1);
         }
     }
 
-
-//    @Override
-//    public <T extends LivingEntity, M extends EntityModel<T>> void render(ItemStack stack,
-//                                                                          SlotContext slotContext, PoseStack matrixStack,
-//                                                                          RenderLayerParent<T, M> renderLayerParent,
-//                                                                          MultiBufferSource renderTypeBuffer, int light,
-//                                                                          float limbSwing, float limbSwingAmount, float partialTicks,
-//                                                                          float ageInTicks, float netHeadYaw, float headPitch) {
-//        LivingEntity entity = slotContext.entity();
+    //broken cock rotation
+//            Quaternionf rotation = new Quaternionf(-1, 0, 0, (headPitch/90)+1);
+//            matrixStack.rotateAround(rotation, 0, 0.5f, 0.1f);
 //
-//
-//        if (entity.level().isClientSide && !Minecraft.getInstance().options.getCameraType().isFirstPerson()) {
-//            ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
-////            BakedModel model1 = itemRenderer.getModel(stack, entity.level(), entity, 0);
-//            BakedModel model = Minecraft.getInstance().getModelManager().getModel(new ResourceLocation("minecraft","item/stick"));
-//            //it can't find the correct model and uses placeholder instead
-//            FiveHundredSnickers.LOGGER.info(String.valueOf(model.equals(Minecraft.getInstance().getModelManager().getMissingModel())));
-//
-//            matrixStack.pushPose();
 //            matrixStack.scale(1, 1, 1);
-////            matrixStack.translate(0, netHeadYaw, headPitch);
-//
-//            itemRenderer.render(stack, ItemDisplayContext.GROUND, false, matrixStack, renderTypeBuffer, light, OverlayTexture.NO_OVERLAY, model);
-//            matrixStack.popPose();
-//        }
-//    }
+//            matrixStack.translate(0, 0.5, 0.1);
+
 }
